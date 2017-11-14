220f889aa638dbfe1f92383636f47e22c965c0be
Alex Cummaudo <alexcu@me.com>
2017-11-14 13:57:43 +1100

    FIX: Fix average sentiment over call script

--- queries/agent-summary/average_sentiment_over_all_calls.sql
+++ queries/agent-summary/average_sentiment_over_all_calls.sql
@@ -1,92 +1,96 @@
--- Need to "pivot" the times from columns to row...
--- Only way to do this in SQL
-
-
-SELECT 0.1
-AS "time", AVG(sentiment_t10) AS "sentiment"
-FROM Call
+SELECT
+  pct AS call_duration,
+  "Overall" AS type,
+  CASE pct
+    WHEN 10 THEN AVG(mono_sentiment_t10)
+    WHEN 20 THEN AVG(mono_sentiment_t20)
+    WHEN 30 THEN AVG(mono_sentiment_t30)
+    WHEN 40 THEN AVG(mono_sentiment_t40)
+    WHEN 50 THEN AVG(mono_sentiment_t50)
+    WHEN 60 THEN AVG(mono_sentiment_t60)
+    WHEN 70 THEN AVG(mono_sentiment_t70)
+    WHEN 80 THEN AVG(mono_sentiment_t80)
+    WHEN 90 THEN AVG(mono_sentiment_t90)
+    WHEN 100 THEN AVG(mono_sentiment_t100)
+  END AS value
+FROM Call, (
+  WITH RECURSIVE Percentiles AS (
+    SELECT 10 AS pct
+    UNION ALL
+    SELECT pct + 10
+    FROM Percentiles
+    WHERE pct < 100
+  )
+  SELECT pct FROM Percentiles
+)
 WHERE call_center_id = {{call_center_id}}
 [[AND agent = {{agent_name}}]]
 [[AND DATE(timestamp) >= {{date_range_start}}]]
 [[AND DATE(timestamp) <= {{date_range_end}}]]
+GROUP BY pct
 
-UNION
+UNION ALL
 
-SELECT 0.2 AS "time", AVG(sentiment_t20) AS "sentiment"
-FROM Call
+SELECT
+  pct AS call_duration,
+  "Client" AS type,
+  CASE pct
+    WHEN 10 THEN AVG(client_sentiment_t10)
+    WHEN 20 THEN AVG(client_sentiment_t20)
+    WHEN 30 THEN AVG(client_sentiment_t30)
+    WHEN 40 THEN AVG(client_sentiment_t40)
+    WHEN 50 THEN AVG(client_sentiment_t50)
+    WHEN 60 THEN AVG(client_sentiment_t60)
+    WHEN 70 THEN AVG(client_sentiment_t70)
+    WHEN 80 THEN AVG(client_sentiment_t80)
+    WHEN 90 THEN AVG(client_sentiment_t90)
+    WHEN 100 THEN AVG(client_sentiment_t100)
+  END AS value
+FROM Call, (
+  WITH RECURSIVE Percentiles AS (
+    SELECT 10 AS pct
+    UNION ALL
+    SELECT pct + 10
+    FROM Percentiles
+    WHERE pct < 100
+  )
+  SELECT pct FROM Percentiles
+)
 WHERE call_center_id = {{call_center_id}}
 [[AND agent = {{agent_name}}]]
 [[AND DATE(timestamp) >= {{date_range_start}}]]
 [[AND DATE(timestamp) <= {{date_range_end}}]]
+GROUP BY pct
 
-UNION
+UNION ALL
 
-SELECT 0.3 AS "time", AVG(sentiment_t30) AS "sentiment"
-FROM Call
+SELECT
+  pct AS call_duration,
+  " Agent" AS type,
+  CASE pct
+    WHEN 10 THEN AVG(agent_sentiment_t10)
+    WHEN 20 THEN AVG(agent_sentiment_t20)
+    WHEN 30 THEN AVG(agent_sentiment_t30)
+    WHEN 40 THEN AVG(agent_sentiment_t40)
+    WHEN 50 THEN AVG(agent_sentiment_t50)
+    WHEN 60 THEN AVG(agent_sentiment_t60)
+    WHEN 70 THEN AVG(agent_sentiment_t70)
+    WHEN 80 THEN AVG(agent_sentiment_t80)
+    WHEN 90 THEN AVG(agent_sentiment_t90)
+    WHEN 100 THEN AVG(agent_sentiment_t100)
+  END AS value
+FROM Call, (
+  WITH RECURSIVE Percentiles AS (
+    SELECT 10 AS pct
+    UNION ALL
+    SELECT pct + 10
+    FROM Percentiles
+    WHERE pct < 100
+  )
+  SELECT pct FROM Percentiles
+)
 WHERE call_center_id = {{call_center_id}}
 [[AND agent = {{agent_name}}]]
 [[AND DATE(timestamp) >= {{date_range_start}}]]
 [[AND DATE(timestamp) <= {{date_range_end}}]]
-
-UNION
-
-SELECT 0.4 AS "time", AVG(sentiment_t40) AS "sentiment"
-FROM Call
-WHERE call_center_id = {{call_center_id}}
-[[AND agent = {{agent_name}}]]
-[[AND DATE(timestamp) >= {{date_range_start}}]]
-[[AND DATE(timestamp) <= {{date_range_end}}]]
-
-UNION
-
-SELECT 0.5 AS "time", AVG(sentiment_t50) AS "sentiment"
-FROM Call
-WHERE call_center_id = {{call_center_id}}
-[[AND agent = {{agent_name}}]]
-[[AND DATE(timestamp) >= {{date_range_start}}]]
-[[AND DATE(timestamp) <= {{date_range_end}}]]
-
-UNION
-
-SELECT 0.6 AS "time", AVG(sentiment_t60) AS "sentiment"
-FROM Call
-WHERE call_center_id = {{call_center_id}}
-[[AND agent = {{agent_name}}]]
-[[AND DATE(timestamp) >= {{date_range_start}}]]
-[[AND DATE(timestamp) <= {{date_range_end}}]]
-
-UNION
-
-SELECT 0.7 AS "time", AVG(sentiment_t70) AS "sentiment"
-FROM Call
-WHERE call_center_id = {{call_center_id}}
-[[AND agent = {{agent_name}}]]
-[[AND DATE(timestamp) >= {{date_range_start}}]]
-[[AND DATE(timestamp) <= {{date_range_end}}]]
-
-UNION
-
-SELECT 0.8 AS "time", AVG(sentiment_t80) AS "sentiment"
-FROM Call
-WHERE call_center_id = {{call_center_id}}
-[[AND agent = {{agent_name}}]]
-[[AND DATE(timestamp) >= {{date_range_start}}]]
-[[AND DATE(timestamp) <= {{date_range_end}}]]
-
-UNION
-
-SELECT 0.9 AS "time", AVG(sentiment_t90) AS "sentiment"
-FROM Call
-WHERE call_center_id = {{call_center_id}}
-[[AND agent = {{agent_name}}]]
-[[AND DATE(timestamp) >= {{date_range_start}}]]
-[[AND DATE(timestamp) <= {{date_range_end}}]]
-
-UNION
-
-SELECT 1.0 AS "time", AVG(sentiment_t100) AS "sentiment"
-FROM Call
-WHERE call_center_id = {{call_center_id}}
-[[AND agent = {{agent_name}}]]
-[[AND DATE(timestamp) >= {{date_range_start}}]]
-[[AND DATE(timestamp) <= {{date_range_end}}]];
+GROUP BY pct;
